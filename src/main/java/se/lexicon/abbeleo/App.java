dfd4fca01ec17149d3c2d5f4f96214c825e85231
leoodindealcantara <leoalcantara@icloud.com>
2021-06-16 13:43:41 +0200

    To do Assignment

--- src/main/java/se/lexicon/abbeleo/App.java
+++ src/main/java/se/lexicon/abbeleo/App.java
@@ -1,13 +1,15 @@
 package se.lexicon.abbeleo;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+public class App {
+    public static void main( String[] args ) {
+
+
+
+
+
+
+
+
     }
 }
