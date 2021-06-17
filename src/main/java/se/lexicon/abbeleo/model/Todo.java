a3ade71d75adb02b7cec4ebd7ac95be01d731765
Abbe-Mohamud <Abbe-Mohamud>
2021-06-17 02:19:10 +0200

    To do Assignment, Packages and Classes added. Test successful (updated).

--- src/main/java/se/lexicon/abbeleo/model/Todo.java
+++ src/main/java/se/lexicon/abbeleo/model/Todo.java
@@ -2,5 +2,51 @@ package se.lexicon.abbeleo.model;
 
 public class Todo {
 
+    private final int todoId;
+    private String description;
+    private boolean done;
+    private Person assignee;
+
+
+
+    public Todo(int todoId, String description){
+
+        this.todoId = todoId;
+        this.description = description;
+    }
+
+
+
+    public int getTodoId() {
+        return todoId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public Person getAssignee() {
+        return assignee;
+    }
+
+    public void setAssignee(Person assignee) {
+        this.assignee = assignee;
+    }
+
+
+
+
 
 }
